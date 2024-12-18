f12fddbdde824f5b7187ca24fefef07f1b77a125
rgsk <guptarahul70322@gmail.com>
2024-12-18 17:40:50 +0530

    fix

--- scripts/restart_postgres.sh
+++ scripts/restart_postgres.sh
@@ -5,7 +5,7 @@ sh "scripts/remove_postgres.sh"
 docker volume create node-ai-experiments-postgres-data
 
 docker run \
-	-p 5465:5432 \
+	-p 5466:5432 \
 	--name node-ai-experiments-postgres \
 	-e POSTGRES_PASSWORD=postgres \
 	-e POSTGRES_USER=postgres \
