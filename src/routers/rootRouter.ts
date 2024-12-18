12b9548af466d168d74ef9f0bb545cf580b44dcc
rgsk <guptarahul70322@gmail.com>
2024-12-18 17:35:29 +0530

    get session

--- src/routers/rootRouter.ts
+++ src/routers/rootRouter.ts
@@ -9,4 +9,29 @@ rootRouter.get("/", async (req, res, next) => {
     message: `Server is running on http://localhost:${environmentVars.PORT}`,
   });
 });
+
+// An endpoint which would work with the client code above - it returns
+// the contents of a REST API request to this protected endpoint
+rootRouter.get("/session", async (req, res, next) => {
+  try {
+    const r = await fetch("https://api.openai.com/v1/realtime/sessions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${environmentVars.OPENAI_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "gpt-4o-realtime-preview-2024-12-17",
+        voice: "verse",
+      }),
+    });
+    const data = await r.json();
+
+    // Send back the JSON we received from the OpenAI REST API
+    return res.json(data);
+  } catch (err) {
+    return next(err);
+  }
+});
+
 export default rootRouter;
