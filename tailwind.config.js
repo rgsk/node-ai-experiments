aa03ef831ace9376b8989dff12a32a8da68512ea
rgsk <guptarahul70322@gmail.com>
2025-01-22 16:00:34 +0530

    font setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./views/*.{ejs,html}"],
 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Figtree", "serif"],
+        mono: ["Dancing Script", "serif"],
+        serif: ["IBM Plex Sans", "serif"],
+      },
+    },
   },
   plugins: [],
 };
