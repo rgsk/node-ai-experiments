73c20b4c0d2eb89deac831fed86adefc116080c4
rgsk <guptarahul70322@gmail.com>
2025-01-27 21:34:46 +0530

    run cpp code

--- src/routers/children/experimentsRouter.ts
+++ src/routers/children/experimentsRouter.ts
@@ -10,23 +10,31 @@ experimentsRouter.post("/execute-code", async (req, res) => {
     node: "node-runner",
     python: "python-runner",
     typescript: "node-runner",
+    cpp: "cpp-runner",
+  };
+  const fileExtensions: Record<string, string> = {
+    node: ".js",
+    python: ".py",
+    typescript: ".ts",
+    cpp: ".cpp",
   };
   const mountPath = path.join(
     process.cwd(),
     "code-runners",
     languageToRunners[language]
   );
-  const tempFileName = "execute-code-temp-file";
+  const tempFileName = `temp${fileExtensions[language]}`;
   const tempFileLocalPath = path.join(mountPath, "src", tempFileName);
   fs.writeFileSync(tempFileLocalPath, code);
 
   const languageToCommands: Record<string, string> = {
-    node: "node",
-    python: "python",
-    typescript: "yarn --silent run:file",
+    node: `node /app/src/${tempFileName}`,
+    python: `python /app/src/${tempFileName}`,
+    typescript: `yarn --silent run:file /app/src/${tempFileName}`,
+    cpp: `bash -c "g++ -o /app/src/temp /app/src/${tempFileName} && /app/src/temp"`,
   };
   // Command to run the Anaconda Docker container and execute the Python script
-  const dockerCommand = `docker run --rm -v ${mountPath}:/app ${languageToRunners[language]} ${languageToCommands[language]} /app/src/${tempFileName}`;
+  const dockerCommand = `docker run --rm -v ${mountPath}:/app ${languageToRunners[language]} ${languageToCommands[language]}`;
 
   // Execute the Docker command
   exec(dockerCommand, (error, stdout, stderr) => {
