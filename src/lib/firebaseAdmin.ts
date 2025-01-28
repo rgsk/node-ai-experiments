8ec5dd9587f54188c85d5ab93bb650e867c86ec5
rgsk <guptarahul70322@gmail.com>
2025-01-28 14:54:54 +0530

    firebase read credentials from json file

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,12 +1,9 @@
 import admin from "firebase-admin";
-import environmentVars from "./environmentVars";
 
 const firebaseAdmin = admin.initializeApp({
-  credential: admin.credential.cert({
-    clientEmail: environmentVars.FIREBASE_CLIENT_EMAIL,
-    privateKey: environmentVars.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    projectId: environmentVars.FIREBASE_PROJECT_ID,
-  }),
+  credential: admin.credential.cert(
+    "credentials/firebaseServiceAccountKey.json"
+  ),
 });
 
 export default firebaseAdmin;
