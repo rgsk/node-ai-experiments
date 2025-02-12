cfa9d2dae28b344bf76228b47435d4ebd34ae76a
rgsk <guptarahul70322@gmail.com>
2025-02-12 15:06:38 +0530

    jest integration

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config(); // .env needs to be loaded otherwise validations in environmentVars.ts file will fail
+
+import type { Config } from "@jest/types";
+
+// Sync object
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  transform: {
+    "ts-jest": [
+      "ts-jest",
+      {
+        diagnostics: {
+          warnOnly: true,
+        },
+      },
+    ],
+  },
+  testEnvironment: "node",
+  moduleDirectories: ["node_modules", "src"],
+  testPathIgnorePatterns: ["tests", "dist"],
+  // tests folder contains graphql related tests which are disabled for now
+  // dist folder is ignored to ensure js equivalents of test files in dist folder don't run
+};
+export default config;
---
+++ src/lib/youtube.test.ts
@@ -0,0 +1,36 @@
+import { chunkWithOverlap } from "./youtube";
+
+test("chunkWithOverlap", () => {
+  expect(
+  chunkWithOverlap([1, 2, 3, 4, 5, 6, 7, 8, 9], 4, 2)
+).toMatchInlineSnapshot(`
+[
+  [
+    1,
+    2,
+    3,
+    4,
+  ],
+  [
+    3,
+    4,
+    5,
+    6,
+  ],
+  [
+    5,
+    6,
+    7,
+    8,
+  ],
+  [
+    7,
+    8,
+    9,
+  ],
+  [
+    9,
+  ],
+]
+`);
+});
