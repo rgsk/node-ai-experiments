0d248613501a68f0dbceaada647ebbb9ad17b212
rgsk <guptarahul70322@gmail.com>
2025-02-21 21:32:39 +0530

    fixes

--- bin.local/start.sh
+++ bin.local/start.sh
@@ -6,7 +6,8 @@ docker run -d \
 --env "HOST_DIR=$(pwd)" \
 --env-file .env \
 -v /var/run/docker.sock:/var/run/docker.sock \
--v $(pwd)/code-runners:/app/code-runners \
+-v ./code-runners:/app/code-runners \
+-v ./secrets:/app/secrets \
 -p 4004:4004 \
 -p 5555:5555 \
 --name node-ai-experiments \
--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -2,7 +2,7 @@ import admin from "firebase-admin";
 
 const firebaseAdmin = admin.initializeApp({
   credential: admin.credential.cert(
-    "credentials-firebaseServiceAccountKey.json"
+    "secrets/credentials-firebaseServiceAccountKey.json"
   ),
 });
 
