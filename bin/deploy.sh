5279590b6bb9336773204b8844001c9aa481705f
rgsk <guptarahul70322@gmail.com>
2025-02-22 23:17:56 +0530

    fix

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,3 @@
-docker build . -t public.ecr.aws/g3x7x7f3/rgskartner/node-ai-experiments:latest --platform linux/amd64
+docker build . -t rgskartner/node-ai-experiments --platform linux/amd64
 
-docker push public.ecr.aws/g3x7x7f3/rgskartner/node-ai-experiments:latest
+docker push rgskartner/node-ai-experiments
