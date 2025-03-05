7a638949ecfb471b3e730d2d54f202ddba53267a
rgsk <guptarahul70322@gmail.com>
2025-03-05 17:40:43 +0530

    mcp example

---
+++ src/examples/mcp.ts
@@ -0,0 +1,56 @@
+import {
+  McpServer,
+  ResourceTemplate,
+} from "@modelcontextprotocol/sdk/server/mcp.js";
+import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
+import express from "express";
+import { z } from "zod";
+
+// Create an MCP server
+const server = new McpServer({
+  name: "Demo",
+  version: "1.0.0",
+});
+
+// Add an addition tool
+server.tool("add", { a: z.number(), b: z.number() }, async ({ a, b }) => ({
+  content: [{ type: "text", text: String(a + b) }],
+}));
+server.tool("multiply", { a: z.number(), b: z.number() }, async ({ a, b }) => ({
+  content: [{ type: "text", text: String(a * b) }],
+}));
+
+// Add a dynamic greeting resource
+server.resource(
+  "greeting",
+  new ResourceTemplate("greeting://{name}", { list: undefined }),
+  async (uri, { name }) => ({
+    contents: [
+      {
+        uri: uri.href,
+        text: `Hello, ${name}!`,
+      },
+    ],
+  })
+);
+
+const app = express();
+
+let transport: SSEServerTransport;
+
+app.get("/sse", async (req, res) => {
+  console.log("Received connection");
+  transport = new SSEServerTransport("/message", res);
+  await server.connect(transport);
+});
+
+app.post("/message", async (req, res) => {
+  console.log("Received message");
+
+  await transport.handlePostMessage(req, res);
+});
+
+const PORT = 8008;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- src/examples/practice.ts
+++ src/examples/practice.ts
@@ -1,25 +1,27 @@
-import { CreditDetails } from "lib/typesJsonData";
-import { jsonDataService } from "routers/children/jsonDataService";
-import { v4 } from "uuid";
-
+import { OpenAIToolSet } from "composio-core";
+import openAIClient from "lib/openAIClient";
 const practice = async () => {
-  await jsonDataService.createMany<CreditDetails>([
-    {
-      key: `reactAIExperiments/admin/public/creditDetails/rahulguptasde@gmail.com`,
-      value: {
-        id: v4(),
-        balance: 100,
-        userEmail: "rahulguptasde@gmail.com",
+  const composioToolset = new OpenAIToolSet({
+    apiKey: "m9szfhv52wlo9kp5s72qh",
+  });
+
+  const tools = await composioToolset.getTools({
+    apps: ["googlesheets"],
+  });
+  const completion = await openAIClient.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      {
+        role: "user",
+        content:
+          "analyse data in the sheet - https://docs.google.com/spreadsheets/d/1cq2PIO46rZo9QWPwfIg167HdkJgeCpBX9RrK3dgy20g/edit?gid=0#gid=0?",
       },
-    },
-    {
-      key: `reactAIExperiments/admin/public/creditDetails/rahulguptacs1@gmail.com`,
-      value: {
-        id: v4(),
-        balance: 100,
-        userEmail: "rahulguptacs1@gmail.com",
-      },
-    },
-  ]);
+    ],
+    tools,
+  });
+
+  console.log(completion.choices[0].message.tool_calls);
+  const resp = await composioToolset.handleToolCall(completion);
+  console.log(resp);
 };
 practice();
