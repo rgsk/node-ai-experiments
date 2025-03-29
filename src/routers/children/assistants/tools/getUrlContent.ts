f60e599442f14add38d55a43e838e08d31617253
rgsk <guptarahul70322@gmail.com>
2025-03-29 17:30:07 +0530

    fix

--- src/routers/children/assistants/tools/getUrlContent.ts
+++ src/routers/children/assistants/tools/getUrlContent.ts
@@ -36,7 +36,13 @@ function isImageUrl(url: string): boolean {
 const headers = {
   "User-Agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
-
+  Accept:
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+  "Accept-Language": "en-US,en;q=0.5",
+  "Accept-Encoding": "gzip, deflate, br",
+  Connection: "keep-alive",
+  Referer: "https://www.espncricinfo.com/",
+  "Upgrade-Insecure-Requests": "1",
   "Sec-Fetch-Dest": "document",
   "Sec-Fetch-Mode": "navigate",
   "Sec-Fetch-Site": "same-origin",
